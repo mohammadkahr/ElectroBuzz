6d79879472e231985a207b98d2cebf0f5caa31e8
mohammadkazem harandi <harandi.mohamma@gmail.com>
2024-04-23 21:32:56 +0330

    The second phase

--- home page/script.js
+++ home page/script.js
@@ -1,25 +1,82 @@
 // Get the modal
 var modal = document.getElementById("loginModal");
+var modal_log = document.getElementById("RegModal");
+var modal_otp = document.getElementById("otpModal");
+var modal_passChange = document.getElementById("passChangeModal");
+var modal_back_login = document.getElementById("background-focuse");
+
+
 
 // Get the button that opens the modal
 var btn = document.getElementById("loginBtn");
+var btn_log = document.getElementById("RegBtn");
+var btn_otp = document.getElementById("OTPBtn");
+var btn_passch = document.getElementById("passChangeBtn");
+var btn_backtol = document.getElementById("Back_to_Login");
+
+
+
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var closeBtnSpan = document.getElementsByClassName("close");
+
+function disableScroll() {
+  document.body.style.overflowY = "hidden";
+}
+
+function enableScroll() {
+  document.body.style.overflowY = "auto";
+}
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
+  modal_back_login.style.display = "block";
   modal.style.display = "block";
+  disableScroll();
 }
+btn_log.onclick = function() {
+  modal.style.display = "none";
+  modal_back_login.style.display = "block";
+  modal_log.style.display = "block";
+  disableScroll();
+}
+btn_otp.onclick = function() {
+  modal.style.display = "none";
+  modal_log.style.display = "none";
+  modal_back_login.style.display = "block";
+  modal_otp.style.display = "block";
+  disableScroll();
+}
+btn_passch.onclick = function() {
+  modal.style.display = "none";
+  modal_log.style.display = "none";
+  modal_back_login.style.display = "block";
+  modal_otp.style.display = "none";
+  modal_passChange.style.display = "block";
+  disableScroll();
+}
+btn_backtol.onclick = function() {
+  modal_log.style.display = "none";
+  modal_otp.style.display = "none";
+  modal_back_login.style.display = "block";
+  modal_passChange.style.display = "none";
+  modal.style.display = "block";
+  disableScroll();
+}
+
+
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target== modal) {
+for (let i = 0; i < closeBtnSpan.length; i++) {
+  closeBtnSpan[i].onclick = function() {
+    // console.log("1");
     modal.style.display = "none";
+    // console.log("2");
+    RegModal.style.display = "none";
+    // console.log("3");
+    otpModal.style.display = "none";
+    // console.log("4");
+    modal_back_login.style.display = "none";
+    enableScroll();
   }
 }
